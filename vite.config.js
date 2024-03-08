6d60cd85ff6b5273d4ba958de16bd06a7d084f45
developermithu <developermithu@gmail.com>
2024-03-08 12:22:22 +0600

    added vite-livewire-plugin to fixed page reload issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
-import laravel, { refreshPaths } from "laravel-vite-plugin";
+import laravel from "laravel-vite-plugin";
+import livewire from "@defstudio/vite-livewire-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: [...refreshPaths, "app/Livewire/**"],
+            refresh: false,
+        }),
+
+        livewire({
+            refresh: ["resources/css/app.css"],
         }),
     ],
 });
